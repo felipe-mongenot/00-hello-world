f0a434b8805a2e0f0428c990662b9aebab43d12d
felipe-mongenot <felipe@joinamply.com>
2024-11-19 18:26:25 -0400

    Initial Commit
    Passed the test!

--- test/00-0-hello-world.spec.js
+++ test/00-0-hello-world.spec.js
@@ -4,7 +4,7 @@ describe("00-0-hello-world", function () {
     spyOn(console, "log").and.callThrough();
   });
   it("should greet with 'Hello, World!'", function () {
-    expect(greet()).toEqual("Hello, World!");
+    expect(greet()).toEqual("Hello, World!!");
   });
   it("should log 'What's up?' to the console", function () {
     logWhatsUp();
